a7d787970898785afc3eeef710ed49b484562594
Fisher Shi <fisher.jianyu.shi@gmail.com>
2017-05-06 15:53:39 -0400

    initialize pid

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,16 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    p_error = 100;
+    i_error = 100;
+    d_error = 100;
 }
 
 void PID::UpdateError(double cte) {
+
 }
 
 double PID::TotalError() {
+
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,10 @@ int main()
 
     PID pid;
     // TODO: Initialize the pid variable.
+    pid.Kp = 0;
+    pid.Ki = 0;
+    pid.Kd = 0;
+    pid.Init(pid.Kp, pid.Ki, pid.Kd);
 
     h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
@@ -64,7 +68,6 @@ int main()
                     json msgJson;
                     msgJson["steering_angle"] = steer_value;
                     msgJson["throttle"] = 0.3;
-
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
                     std::cout << msg << std::endl;
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
